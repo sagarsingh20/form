68e13064a854f1201e244ff378ac6bbd9b7ca23f
admin <shayna.raj@rootinfosol.com>
2020-09-11 11:01:36 +0530

    my project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'form';
-}
+export class AppComponent implements OnInit {
+ dataa = [{id: ''}];
+  check=[{id:''}];
+  sel=[{id:''}]
+ ngOnInit(){
+   
+ }
+  in(){
+    this.dataa.push({id: ''});
+   }
+   inn(){
+    this.check.push({id:''});
+   }
+   innn(){
+    this.sel.push({id:''})
+   }
+
+   out(i){
+     this.dataa.splice(i,1);
+   }
+   outt(i){
+    this.check.splice(i,1);
+   }
+   outtt(i){
+    this.sel.splice(i,1);
+   }
+    
+  }
